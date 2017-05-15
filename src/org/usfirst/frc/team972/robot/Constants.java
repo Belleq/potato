46c6b63d9ac696de0e5367eda38bcf519a3aa671
Jerry Zhang <jerry.w.zhang@gmail.com>
2017-05-15 15:45:12 -0700

    Drive code

---
+++ src/org/usfirst/frc/team972/robot/Constants.java
@@ -0,0 +1,6 @@
+package org.usfirst.frc.team972.robot;
+
+public class Constants {
+	public static final int REVERSE_BUTTON_LEFTJOY = 1;
+	public static final int SQUARE_TOGGLE_BUTTON_LEFTJOY = 2;
+}
--- src/org/usfirst/frc/team972/robot/Robot.java
+++ src/org/usfirst/frc/team972/robot/Robot.java
@@ -1,80 +1,64 @@
 package org.usfirst.frc.team972.robot;
 
-import edu.wpi.first.wpilibj.IterativeRobot;
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.*;
+import com.ctre.*;
 
-/**
- * The VM is configured to automatically run this class, and to call the
- * functions corresponding to each mode, as described in the IterativeRobot
- * documentation. If you change the name of this class or the package after
- * creating this project, you must also update the manifest file in the resource
- * directory.
- */
 public class Robot extends IterativeRobot {
-	final String defaultAuto = "Default";
-	final String customAuto = "My Auto";
-	String autoSelected;
-	SendableChooser<String> chooser = new SendableChooser<>();
-
-	/**
-	 * This function is run when the robot is first started up and should be
-	 * used for any initialization code.
-	 */
-	@Override
-	public void robotInit() {
-		chooser.addDefault("Default Auto", defaultAuto);
-		chooser.addObject("My Auto", customAuto);
-		SmartDashboard.putData("Auto choices", chooser);
-	}
-
-	/**
-	 * This autonomous (along with the chooser code above) shows how to select
-	 * between different autonomous modes using the dashboard. The sendable
-	 * chooser code works with the Java SmartDashboard. If you prefer the
-	 * LabVIEW Dashboard, remove all of the chooser code and uncomment the
-	 * getString line to get the auto name from the text box below the Gyro
-	 *
-	 * You can add additional auto modes by adding additional comparisons to the
-	 * switch structure below with additional strings. If using the
-	 * SendableChooser make sure to add them to the chooser code above as well.
-	 */
-	@Override
-	public void autonomousInit() {
-		autoSelected = chooser.getSelected();
-		// autoSelected = SmartDashboard.getString("Auto Selector",
-		// defaultAuto);
-		System.out.println("Auto selected: " + autoSelected);
-	}
-
-	/**
-	 * This function is called periodically during autonomous
-	 */
-	@Override
-	public void autonomousPeriodic() {
-		switch (autoSelected) {
-		case customAuto:
-			// Put custom auto code here
-			break;
-		case defaultAuto:
-		default:
-			// Put default auto code here
-			break;
+	CANTalon leftMotor = new CANTalon(1);
+	CANTalon rightMotor = new CANTalon(2);
+	
+	Joystick leftJoy = new Joystick(0);
+	Joystick rightJoy = new Joystick(1);
+	
+	// reverse drive + squared inputs
+	boolean reversed = false;
+	boolean squared = false;
+	
+	boolean lastSqu = false;
+	
+	double comp(double x){
+		// so you don't have to retype it for both
+		x = ((Math.min(0.0,(Math.abs(x) - 0.05)) * x) / Math.abs(x)) / 0.95; // 5% buffer
+		
+		if (reversed){
+			x *= -1;
 		}
+		
+		if (squared){
+			x *= Math.abs(x);
+		}
+		
+		return x;
+	}
+	
+	public void robotInit() {
+		
 	}
 
-	/**
-	 * This function is called periodically during operator control
-	 */
-	@Override
+	
+	public void autonomousInit() {
+		
+	}
+
+	public void autonomousPeriodic() {
+		
+	}
+
 	public void teleopPeriodic() {
+		reversed = leftJoy.getRawButton(Constants.REVERSE_BUTTON_LEFTJOY);
+		boolean currSqu = leftJoy.getRawButton(Constants.SQUARE_TOGGLE_BUTTON_LEFTJOY);
+		
+		if (currSqu && !lastSqu){
+			squared = !squared;
+		}
+		lastSqu = currSqu;
+		
+		leftMotor.set(comp(leftJoy.getY()));
+		rightMotor.set(comp(rightJoy.getY()));
 	}
 
-	/**
-	 * This function is called periodically during test mode
-	 */
-	@Override
 	public void testPeriodic() {
+		
 	}
 }
 
